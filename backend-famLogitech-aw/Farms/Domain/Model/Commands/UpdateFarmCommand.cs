d97049458a50485378409fade09cf1865f455590
Rod Aguilar <108448668+Rodrag0lvr@users.noreply.github.com>
2024-05-22 17:34:27 -0500

    fix: Fixed updated command

--- backend-famLogitech-aw/Farms/Domain/Model/Commands/UpdateFarmCommand.cs
+++ backend-famLogitech-aw/Farms/Domain/Model/Commands/UpdateFarmCommand.cs
@@ -1,13 +1,3 @@
 namespace backend_famLogitech_aw.Farms.Domain.Model.Commands;
 
-public record class UpdateFarmCommand(int resourceId, string resourceFarmName, string resourceLocation, string resourceType, string resourceInfrastructure, string resourceCertificate, string resourceProduct)
-{
-    public int Id { get; private set; }
-    public string FarmName { get; set; }
-    public string Location { get; set; }
-    public string Type { get; set; }
-    public string Infrastructure { get; set; } 
-    public string Certificate { get; set; }
-    public string Product { get; set; }
-
-}
+public record UpdateFarmCommand(int Id,string FarmName, string Location, string Type, string Infrastructure, string Certificate, string Product);
