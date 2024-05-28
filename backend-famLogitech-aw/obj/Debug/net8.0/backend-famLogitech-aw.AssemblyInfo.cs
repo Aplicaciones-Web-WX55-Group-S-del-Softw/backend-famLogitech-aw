d46399c05eb1dd52c44aafdeb50ea89216e8b56b
Giacomo202210029 <u202210029@upc.edu.pe>
2024-05-27 23:17:40 -0500

    fix:  fix problems?

--- backend-famLogitech-aw/obj/Debug/net8.0/backend-famLogitech-aw.AssemblyInfo.cs
+++ backend-famLogitech-aw/obj/Debug/net8.0/backend-famLogitech-aw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-famLogitech-aw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a45de7646f8e525a8a5a77e0b48fd15b7c440a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea165a0db79a913b220f9d40dc895d60bd95628c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-famLogitech-aw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-famLogitech-aw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
