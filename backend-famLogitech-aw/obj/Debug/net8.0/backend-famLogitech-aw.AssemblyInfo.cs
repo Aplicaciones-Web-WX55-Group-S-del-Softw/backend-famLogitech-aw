3fa1bc2c123bcb6d7e64c2f1a2bdbccf937d3e46
Rodrag0lvr <crodrigo7132@gmail.com>
2024-06-07 01:22:34 -0500

    feat: add changes.

--- backend-famLogitech-aw/obj/Debug/net8.0/backend-famLogitech-aw.AssemblyInfo.cs
+++ backend-famLogitech-aw/obj/Debug/net8.0/backend-famLogitech-aw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-famLogitech-aw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae72c15a53c17cc6ab065181f2b359fb42588c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e141a67e2fe349a7f78a3c2ebb76ce7cfa8918ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-famLogitech-aw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-famLogitech-aw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
